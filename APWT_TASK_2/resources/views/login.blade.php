5987d24a18a4061c84befec89061807b85f5bfcc
Samiha Hossain <75538711+Samiha-Hossain19@users.noreply.github.com>
2022-10-17 10:02:30 +0600

    View Change

--- APWT_TASK_2/resources/views/login.blade.php
+++ APWT_TASK_2/resources/views/login.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
     <body>
@@ -19,7 +21,7 @@
             <h3>LOGIN</h3><br>
             <form action="/login" method="POST">
             @csrf {{ csrf_field() }}
-            <div>
+            <div class="form-group">
             <div><label>ID:</label></div>
             <div>
             <input type="text" placeholder="Enter your id" name="id"/>
@@ -28,7 +30,7 @@
             @enderror
             </div>
             </div><br>
-            <div>
+            <div class="form-group">
             <div>
             <label>PASSWORD: </label>
             <div>
@@ -38,10 +40,12 @@
             @enderror
             </div>
             </div><br>
-            <div>
+            <div class="form-group">
             <button name="name">Submit</button>
             </div>
             </form>
         </div>
+        <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>
